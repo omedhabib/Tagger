c64402400badfd11dc2a71b118bb5bf18c681114
Omed Habib <omed@habib.io>
2023-11-21 10:00:51 -0800

    Update tagger.js
    Added the ability to collect IP addresses

--- tagger.js
+++ tagger.js
@@ -1,21 +1,11 @@
 <script>
-  
-/*
-  Created by Omed Habib
-  Email: omed@habib.io
-  Project: https://github.com/omedhabib/Tagger
+// Tagger.js - A script for capturing URL parameters and setting them as cookies.
+// It then dynamically inserts these values, along with the user's IP address, into hidden fields in all forms on a webpage.
 
-  Tagger is designed to capture specific URL parameters like 'utm_medium', 'utm_source', etc., and store them as cookies with custom names. These values are then dynamically inserted into hidden input fields in all forms on a webpage. This enables marketers, sales teams, and affiliate partners to track and attribute user behavior from different campaigns and sources.
-
-  Example use cases include proper attribution for campaigns, affiliate relationships, sales efforts, and more. The default cookie expiration is set to one month but can be customized as needed.
-
-  Note: This data is captured through form submissions, and original sources may be overwritten in cookies if users return from different sources. Use this data with discretion.
-*/
-
-// Cookie expiration in days
+// Define the cookie expiration in days.
 var cookieExpirationDays = 30;
 
-// Mapping of URL parameters to custom cookie names
+// Define the mapping of specific URL parameters to custom cookie names.
 var specificParameters = {
   'utm_medium': 'channel',
   'utm_source': 'program',
@@ -25,7 +15,7 @@ var specificParameters = {
   'ref': 'ref'
 };
 
-// Function to get URL parameters and return them as an object
+// Function to parse URL parameters and return them as an object.
 function getURLParameters(url) {
   var params = {};
   var parser = document.createElement('a');
@@ -41,7 +31,7 @@ function getURLParameters(url) {
   return params;
 }
 
-// Function to set a cookie
+// Function to set a cookie.
 function setCookie(name, value, days) {
   var expires = "";
   if (days) {
@@ -52,7 +42,7 @@ function setCookie(name, value, days) {
   document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 
-// Capture URL parameters and set them as cookies with custom names
+// Capture URL parameters and set them as cookies with custom names.
 var params = getURLParameters(window.location.href);
 for (var key in specificParameters) {
   if (params[key]) {
@@ -60,7 +50,7 @@ for (var key in specificParameters) {
   }
 }
 
-// Function to dynamically insert hidden input fields into all forms
+// Function to dynamically insert hidden fields into all forms.
 function insertHiddenFields() {
   var forms = document.getElementsByTagName('form');
   for (var i = 0; i < forms.length; i++) {
@@ -68,46 +58,41 @@ function insertHiddenFields() {
     for (var key in specificParameters) {
       var hiddenInput = document.createElement('input');
       hiddenInput.type = 'hidden';
-      hiddenInput.name = specificParameters[key]; // Using the custom cookie name as the input name
+      hiddenInput.name = specificParameters[key];
       form.appendChild(hiddenInput);
     }
+    
+    // Insert a hidden field for the IP address.
+    var ipInput = document.createElement('input');
+    ipInput.type = 'hidden';
+    ipInput.name = 'user_ip';
+    form.appendChild(ipInput);
   }
 }
 
-// Call the function to insert hidden fields
-insertHiddenFields();
-
-// Function to get a cookie by name
-function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-  }
-  return null;
-}
-
-// Function to fill hidden fields with corresponding cookie values
-function fillHiddenFields() {
-  var forms = document.getElementsByTagName('form');
-  for (var i = 0; i < forms.length; i++) {
-    var form = forms[i];
-    var hiddenFields = form.querySelectorAll('input[type="hidden"]');
-    for (var j = 0; j < hiddenFields.length; j++) {
-      var field = hiddenFields[j];
-      var cookieName = specificParameters[field.name];
-      if (cookieName) {
-        var cookieValue = getCookie(cookieName);
-        if (cookieValue) {
-          field.value = cookieValue;
+// Function to make an AJAX call to retrieve the user's IP address.
+function getUserIP() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var ip = JSON.parse(this.responseText).ip;
+      // Populate the IP address in each form's hidden field.
+      var forms = document.getElementsByTagName('form');
+      for (var i = 0; i < forms.length; i++) {
+        var form = forms[i];
+        var ipField = form.querySelector('input[name="user_ip"]');
+        if (ipField) {
+          ipField.value = ip;
         }
       }
     }
-  }
+  };
+  xhr.open("GET", "https://api.ipify.org?format=json", true);
+  xhr.send();
 }
 
-// Call the function to fill hidden fields with cookie values
-fillHiddenFields();
+// Insert hidden fields and retrieve the user's IP when the page loads.
+insertHiddenFields();
+getUserIP();
+  
 </script>  
